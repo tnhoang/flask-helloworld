84397806f9f98d5e06e6cddf5d1c95f89b559fba
tnhoang <tnhoang088@gmail.com>
2021-07-29 17:17:28 +0700

    update run script

--- src/main.py
+++ src/main.py
@@ -6,13 +6,13 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
-  def hello(self):
-    return {'hello': 'world'}
+    def get(self):
+        return {'hello': 'world'}
+
 
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
-
-
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
